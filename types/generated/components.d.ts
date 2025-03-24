e9e1b9a9cfbe4b10d2cc955b453b7aaef0060d71
pankajpal05 <pankaj.pal@ayasya.com>
2025-03-24 15:33:43 +0530

    update the strapi backend

---
+++ types/generated/components.d.ts
@@ -0,0 +1,223 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface CarrersComponentCarrersComponent
+  extends Struct.ComponentSchema {
+  collectionName: 'components_carrers_component_carrers_components';
+  info: {
+    description: '';
+    displayName: 'Carrers Component';
+  };
+  attributes: {
+    carrerTeaser: Schema.Attribute.Component<'teaser.teaser', true>;
+    heading: Schema.Attribute.String;
+    slider: Schema.Attribute.Component<'teaser.teaser', true>;
+  };
+}
+
+export interface CtaButtonCtaButton extends Struct.ComponentSchema {
+  collectionName: 'components_cta_button_cta_buttons';
+  info: {
+    description: '';
+    displayName: 'CTA Button';
+  };
+  attributes: {
+    Icon: Schema.Attribute.Media<'images'>;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface DescriptionDescription extends Struct.ComponentSchema {
+  collectionName: 'components_description_descriptions';
+  info: {
+    description: '';
+    displayName: 'Description';
+  };
+  attributes: {
+    description: Schema.Attribute.Text & Schema.Attribute.Required;
+  };
+}
+
+export interface ExperienceComponentExperienceComponent
+  extends Struct.ComponentSchema {
+  collectionName: 'components_experience_component_experience_components';
+  info: {
+    description: '';
+    displayName: 'Experience Component';
+  };
+  attributes: {
+    metricSection: Schema.Attribute.Component<'metric-card.metric-card', true>;
+    teaser: Schema.Attribute.Component<'teaser.teaser', false>;
+  };
+}
+
+export interface FooterTaglineFooterTagline extends Struct.ComponentSchema {
+  collectionName: 'components_footer_tagline_footer_taglines';
+  info: {
+    displayName: 'Footer Tagline';
+  };
+  attributes: {
+    label: Schema.Attribute.Component<'title.title', true>;
+  };
+}
+
+export interface FooterFooter extends Struct.ComponentSchema {
+  collectionName: 'components_footer_footers';
+  info: {
+    description: '';
+    displayName: 'Footer';
+  };
+  attributes: {
+    address: Schema.Attribute.Text;
+    logo: Schema.Attribute.Media<'images', true> & Schema.Attribute.Required;
+    tagLine: Schema.Attribute.String;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface HeroBannerHeroBanner extends Struct.ComponentSchema {
+  collectionName: 'components_hero_banner_hero_banners';
+  info: {
+    description: '';
+    displayName: 'Hero Banner';
+  };
+  attributes: {};
+}
+
+export interface ImageImage extends Struct.ComponentSchema {
+  collectionName: 'components_image_images';
+  info: {
+    displayName: 'Image';
+  };
+  attributes: {
+    image: Schema.Attribute.Media<'images', true> & Schema.Attribute.Required;
+  };
+}
+
+export interface LatestSectionLatestSection extends Struct.ComponentSchema {
+  collectionName: 'components_latest_section_latest_sections';
+  info: {
+    description: '';
+    displayName: 'Latest Section';
+  };
+  attributes: {
+    heading: Schema.Attribute.String;
+    newsTeaser: Schema.Attribute.Component<'teaser.teaser', true>;
+    socialTeaser: Schema.Attribute.Component<'teaser.teaser', true>;
+  };
+}
+
+export interface MenuItemMenuItem extends Struct.ComponentSchema {
+  collectionName: 'components_menu_item_menu_items';
+  info: {
+    description: '';
+    displayName: 'Menu Item';
+  };
+  attributes: {
+    openInNewTab: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    title: Schema.Attribute.String;
+    url: Schema.Attribute.String;
+  };
+}
+
+export interface MenuMenu extends Struct.ComponentSchema {
+  collectionName: 'components_menu_menus';
+  info: {
+    description: '';
+    displayName: 'Menu';
+  };
+  attributes: {
+    children: Schema.Attribute.Component<'menu-item.menu-item', true>;
+    openInNewTab: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    title: Schema.Attribute.String;
+    url: Schema.Attribute.String;
+  };
+}
+
+export interface MetricCardMetricCard extends Struct.ComponentSchema {
+  collectionName: 'components_metric_card_metric_cards';
+  info: {
+    description: '';
+    displayName: 'Metric Card';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    subTitle: Schema.Attribute.String & Schema.Attribute.Required;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface TeaserTeaser extends Struct.ComponentSchema {
+  collectionName: 'components_teaser_teasers';
+  info: {
+    description: '';
+    displayName: 'Teaser';
+  };
+  attributes: {
+    cta: Schema.Attribute.Component<'cta-button.cta-button', false>;
+    description: Schema.Attribute.Text;
+    image: Schema.Attribute.Media<'images', true>;
+    tag: Schema.Attribute.String;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface TitleTitle extends Struct.ComponentSchema {
+  collectionName: 'components_title_titles';
+  info: {
+    description: '';
+    displayName: 'Title';
+  };
+  attributes: {
+    title: Schema.Attribute.Text & Schema.Attribute.Required;
+  };
+}
+
+export interface VideoTeaserVideoTeaser extends Struct.ComponentSchema {
+  collectionName: 'components_video_teaser_video_teasers';
+  info: {
+    description: '';
+    displayName: 'Video Teaser';
+  };
+  attributes: {
+    cta: Schema.Attribute.Component<'cta-button.cta-button', false>;
+    description: Schema.Attribute.Text;
+    tag: Schema.Attribute.String;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
+    videoMedia: Schema.Attribute.Media<'videos', true> &
+      Schema.Attribute.Required;
+  };
+}
+
+export interface VideoVideo extends Struct.ComponentSchema {
+  collectionName: 'components_video_videos';
+  info: {
+    description: '';
+    displayName: 'Video';
+  };
+  attributes: {
+    video: Schema.Attribute.Media<'videos', true> & Schema.Attribute.Required;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'carrers-component.carrers-component': CarrersComponentCarrersComponent;
+      'cta-button.cta-button': CtaButtonCtaButton;
+      'description.description': DescriptionDescription;
+      'experience-component.experience-component': ExperienceComponentExperienceComponent;
+      'footer-tagline.footer-tagline': FooterTaglineFooterTagline;
+      'footer.footer': FooterFooter;
+      'hero-banner.hero-banner': HeroBannerHeroBanner;
+      'image.image': ImageImage;
+      'latest-section.latest-section': LatestSectionLatestSection;
+      'menu-item.menu-item': MenuItemMenuItem;
+      'menu.menu': MenuMenu;
+      'metric-card.metric-card': MetricCardMetricCard;
+      'teaser.teaser': TeaserTeaser;
+      'title.title': TitleTitle;
+      'video-teaser.video-teaser': VideoTeaserVideoTeaser;
+      'video.video': VideoVideo;
+    }
+  }
+}
